0e2ed972dfd58a6d1d14f440180fd8c17f43cf95
CA Womack <cawomack@Mac.localdomain>
2026-04-08 12:55:25 -0500

    final fix

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const MidiWriter = require("midi-writer-js");
-const fs = require("fs");
 const cors = require("cors");
+const fs = require("fs");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+// CREATE MIDI ENDPOINT
 app.post("/generate-midi", (req, res) => {
   const { bpm, files } = req.body;
-
   let output = {};
 
   for (let name in files) {
@@ -19,7 +19,7 @@ app.post("/generate-midi", (req, res) => {
     files[name].forEach(n => {
       track.addEvent(new MidiWriter.NoteEvent({
         pitch: [n.note],
-        duration: 'T' + Math.floor(n.duration * 128),
+        duration: "T" + Math.floor(n.duration * 128),
         startTick: Math.floor(n.start * 128),
         velocity: n.velocity
       }));
@@ -30,14 +30,20 @@ app.post("/generate-midi", (req, res) => {
 
     fs.writeFileSync(`./midis/${fileName}`, writer.buildFile());
 
-    output[name] = `http://localhost:3000/midis/${fileName}`;
+    output[name] = `${BASE_URL}/midis/${fileName}`;
   }
 
   res.json(output);
 });
 
+// SERVE MIDI FILES
 app.use("/midis", express.static("midis"));
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+// PORT + URL CONFIG
+const PORT = process.env.PORT || 3000;
+const BASE_URL = process.env.BASE_URL || `http://localhost:${PORT}`;
+
+// START SERVER
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
